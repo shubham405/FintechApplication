15bf51c261d542e3103dfe82e1ea8374e47dbe02
Shubham <shubhamraj405@gmail.com>
2023-09-13 13:27:29 +0530

    done

--- Fintech_app/settings.py
+++ Fintech_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#50u4@45bs=l%-jye%#f%y+8qg4r6mrt1&emw-ul5l%=gdgxy@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG',"False").lower()=="true"
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
